d94b7020b144add8a65ab0615a847f214f7f1b22
Hafiz Naufal Rahman <hafiz.nr5758@gmail.com>
2019-04-25 08:47:59 +0700

    commit

--- src/main.js
+++ src/main.js
@@ -7,12 +7,7 @@ import firebase from 'firebase'
 // Required for side-effects
 require("firebase/firestore");
 var config = {
-  apiKey: "AIzaSyDNtzxh7aiegFVdp9rLgn2WXnlu6MAhCQw",
-  authDomain: "petsin-245f5.firebaseapp.com",
-  databaseURL: "https://petsin-245f5.firebaseio.com",
-  projectId: "petsin-245f5",
-  storageBucket: "petsin-245f5.appspot.com",
-  messagingSenderId: "946473095361"
+ 
 };
 firebase.initializeApp(config);
 
