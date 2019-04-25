00d51dc6ff9432357399fa518f49d52aa806dfb2
Hafiz Naufal Rahman <hafiz.nr5758@gmail.com>
2019-04-25 08:33:14 +0700

    commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+// Required for side-effects
+require("firebase/firestore");
+var config = {
+  apiKey: "AIzaSyDNtzxh7aiegFVdp9rLgn2WXnlu6MAhCQw",
+  authDomain: "petsin-245f5.firebaseapp.com",
+  databaseURL: "https://petsin-245f5.firebaseio.com",
+  projectId: "petsin-245f5",
+  storageBucket: "petsin-245f5.appspot.com",
+  messagingSenderId: "946473095361"
+};
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+window.db= db;
+
+//disable deprecated
+db.settings({
+  timestampsInSnapshots: true
+})
 
 Vue.config.productionTip = false
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import PrivateChat from './views/PrivateChat.vue'
+import Login from './views/Login.vue'
+
 
 Vue.use(Router)
 
@@ -11,7 +14,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: PrivateChat
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/about',
